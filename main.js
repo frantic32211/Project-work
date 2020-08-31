51e2dd1e97248b0f746ea65584f985302da9c0e8
Shashikant Mishra <26shashimishra@gmail.com>
2020-08-31 22:04:51 +0530

    js file added

---
+++ main.js
@@ -0,0 +1,3 @@
+var info = document.querySelector("h2")
+
+info.textContent = "Javascript is a dynamic programming language that can add functionality and interactivity to a website. It has const, let and var datatypes which can store all type of values like integer, float, boolean, array,objects, etc."
