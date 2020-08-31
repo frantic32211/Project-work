3897d8252f256a0e00895aef8ecb0b47d294c7f3
Shashikant Mishra <26shashimishra@gmail.com>
2020-09-01 03:11:11 +0530

    math functions added

---
+++ mainTwo.js
@@ -0,0 +1,17 @@
+console.log(Math.pow(2,5));
+
+console.log(Math.sqrt(256));
+
+console.log(Math.abs(-142.63));
+
+console.log(Math.random()*5);
+
+console.log(Math.round(465.64));
+
+console.log(Math.floor(Math.random()*5));
+
+console.log(Math.ceil(Math.random()*5));
+
+console.log(Math.sin(90 * Math.PI / 180));
+
+console.log(Math.cos(0 * Math.PI / 180));
