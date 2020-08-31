fa9f256e2562cd5395a5e5d73e3ea706448038e1
Shashikant Mishra <26shashimishra@gmail.com>
2020-08-31 22:43:31 +0530

    array printed

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 var info = document.querySelector("h2")
 
 info.textContent = "Javascript is a dynamic programming language that can add functionality and interactivity to a website. It has const, let and var datatypes which can store all type of values like integer, float, boolean, array,objects, etc."
+
+var printArray = document.querySelector("h1")
+
+const array = [1,2,3,4,5]
+
+let value = array[3].toString()
+printArray.innerHTML = value
+
+console.log(`array at index 3 is a ${typeof value}`);
