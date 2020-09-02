ff7641d223b871d3fec0d18900fa96c6625ce0f9
Shashikant Mishra <26shashimishra@gmail.com>
2020-09-02 05:46:32 +0530

    form validation added

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-var submitPage = document.querySelector("#formFill");
+var nameOfUser = document.querySelector("#Name");
+var phoneOfUser = document.querySelector("#phoneNumber");
+
+var resName = /[a-zA-Z]+/.test(nameOfUser.value);
+var resPhone = /\d+/.test(phoneOfUser.value);
+
+var checkd = document.getElementById("Physics").checked || document.getElementById("Chemistry").checked || document.getElementById("Biology").checked || document.getElementById("Maths").checked
+
+
+var submitPage = document.querySelector("#formFill")
+
 submitPage.addEventListener("submit", (e) => {
     e.preventDefault();
-})
 
-var nameOfUser = document.querySelector("#Name");
-
-function myfun() {
-    var re = /^[a-zA-Z]+/;
-    var res = re.test(nameOfUser.value);
-    if (!res) {
-         return alert("Only alphabets allowed!");
+    if (resName && resPhone && checkd ){
+        console.log(checkd);
+        alert("Your registration has be submitted successfully!")
+        nameOfUser = ""
+        phoneOfUser = ""
+    }else {
+        alert("Please enter necessary credentials!")
     }
-
-}
+})
 
