6827bc630ba60c9ddd202df8ff9e7835c33fa5cd
Shashikant Mishra <26shashimishra@gmail.com>
2020-09-02 23:33:10 +0530

    onclick added

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
-var nameOfUser = document.querySelector("#Name");
-var phoneOfUser = document.querySelector("#phoneNumber");
+function pageRedirectOne() {
+    window.location.href = "firstHTML.html";
+  }
+  
+function pageRedirectTwo() {
+    window.location.href = "courses.html";
+  }
 
-var resName = /[a-zA-Z]+/.test(nameOfUser.value);
-var resPhone = /\d+/.test(phoneOfUser.value);
+function pageRedirectThree() {
+    window.location.href = "submitted.html";
+  }
 
-var checkd = document.getElementById("Physics").checked || document.getElementById("Chemistry").checked || document.getElementById("Biology").checked || document.getElementById("Maths").checked
+
+// var nameOfUser = document.querySelector("#Name");
+// var phoneOfUser = document.querySelector("#phoneNumber");
+
+// var resName = /[a-zA-Z]+/.test(nameOfUser.value);
+// var resPhone = /\d+/.test(phoneOfUser.value);
+
+// var checkd = document.getElementById("Physics").checked || document.getElementById("Chemistry").checked || document.getElementById("Biology").checked || document.getElementById("Maths").checked
 
 
-var submitPage = document.querySelector("#formFill")
+// var submitPage = document.querySelector("#formFill")
 
-submitPage.addEventListener("submit", (e) => {
-    e.preventDefault();
+// submitPage.addEventListener("submit", (e) => {
+//     e.preventDefault();
 
-    if (resName && resPhone && checkd ){
-        console.log(checkd);
-        alert("Your registration has be submitted successfully!")
-        nameOfUser = ""
-        phoneOfUser = ""
-    }else {
-        alert("Please enter necessary credentials!")
-    }
-})
+//     if (resName && resPhone && checkd ){
+//         console.log(checkd);
+//         alert("Your registration has be submitted successfully!")
+//         nameOfUser = ""
+//         phoneOfUser = ""
+//     }else {
+//         alert("Please enter necessary credentials!")
+//     }
+// })
 
